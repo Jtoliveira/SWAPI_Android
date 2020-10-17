868a7df01654ee573fcea7c67ad2379436cc7d53
João Oliveira <joaotiagomingachos@hotmail.com>
2020-10-17 22:44:37 +0100

    fullscreen

--- app/src/main/java/com/example/swapi/MainActivity.java
+++ app/src/main/java/com/example/swapi/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideSystemUI();
 
         mainGrid = (GridLayout) findViewById(R.id.mainGrid);
 
@@ -315,4 +316,23 @@ public class MainActivity extends AppCompatActivity {
         }
         return "";
     }
+
+    //method from https://developer.android.com/training/system-ui/immersive#java to make the app fullscreen
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 }
